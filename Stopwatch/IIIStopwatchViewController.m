c22e1850880c261675673c1c0f45ec74f33b03f7
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-11-27 12:39:07 -0600

    IIIStopwatchViewController actions basic func completed

--- Stopwatch/IIIStopwatchViewController.m
+++ Stopwatch/IIIStopwatchViewController.m
@@ -7,24 +7,37 @@
 //
 
 #import "IIIStopwatchViewController.h"
+#import "IIIStopwatch.h"
 
 @interface IIIStopwatchViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *timeLabel;
 @property (weak, nonatomic) IBOutlet UIButton *resetButton;
 @property (weak, nonatomic) IBOutlet UIButton *startStopButton;
 
+@property (nonatomic, strong) IIIStopwatch *stopwatch;
+
 @end
 
 @implementation IIIStopwatchViewController
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.stopwatch = [[IIIStopwatch alloc] init];
+}
+
 - (IBAction)reset:(id)sender
 {
-    
+    if (self.stopwatch.isRunning) {
+        [self.stopwatch stop];
+    } else {
+        [self.stopwatch start];
+    }
 }
 
 - (IBAction)startOrStop:(id)sender
 {
-    
+    [self.stopwatch reset];
 }
 
 @end
