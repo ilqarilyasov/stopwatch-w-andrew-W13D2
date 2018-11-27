088464d8d47a846532592c45df8788ab995279f4
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-11-27 12:48:26 -0600

    updateViews completed

--- Stopwatch/IIIStopwatchViewController.m
+++ Stopwatch/IIIStopwatchViewController.m
@@ -20,11 +20,17 @@
 
 @implementation IIIStopwatchViewController
 
+// MARK: - Lifecycle
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     self.stopwatch = [[IIIStopwatch alloc] init];
+    [self updateViews];
 }
+
+// MARK: - Actions
 
 - (IBAction)reset:(id)sender
 {
@@ -38,6 +44,31 @@
 - (IBAction)startOrStop:(id)sender
 {
     [self.stopwatch reset];
+}
+
+// MARK: - Private
+
+- (void)updateViews
+{
+    self.timeLabel.text = [self stringFromTimeInterval:self.stopwatch.elapsedTime];
+    
+    if (self.stopwatch.isRunning) {
+        [self.startStopButton setTitle:@"Stop" forState:UIControlStateNormal];
+    } else {
+        [self.startStopButton setTitle:@"Start" forState:UIControlStateNormal];
+    }
+    
+    self.resetButton.enabled = self.stopwatch.elapsedTime > 0;
+}
+
+- (NSString *)stringFromTimeInterval:(NSTimeInterval)interval
+{
+    NSInteger timeIntervalAsInt = (NSInteger)interval;
+    NSInteger tenths = (NSInteger)((interval - floor(interval)) * 10);
+    NSInteger seconds = timeIntervalAsInt % 60;
+    NSInteger minutes = (timeIntervalAsInt / 60) % 60;
+    NSInteger hours = (timeIntervalAsInt / 3600);
+    return [NSString stringWithFormat:@"%02ld:%02ld:%02ld.%ld", (long)hours, (long)minutes, (long)seconds, (long)tenths];
 }
 
 @end
